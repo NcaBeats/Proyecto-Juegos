91ff03617f0fef03a1a21711e16f597aba0a5479
Nicolás Carvajal <nicocarvajal900@gmail.com>
2026-04-21 20:35:42 -0400

    Primer Commit

---
+++ ms-juego/src/main/java/com/app/msjuego/MsJuegoApplication.java
@@ -0,0 +1,13 @@
+package com.app.msjuego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJuegoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsJuegoApplication.class, args);
+    }
+
+}

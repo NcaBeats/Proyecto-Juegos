1192188499223b5ac08ffddc9bdd555e253a0c07
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-11 12:00:14 -0400

    Eureka Server Implementado

---
+++ ms-eureka/src/test/java/com/example/mseureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mseureka;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+    @org.testng.annotations.Test
+    void contextLoads() {
+    }
+
+}
